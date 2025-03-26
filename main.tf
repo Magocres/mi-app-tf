37a3e4e4b52ae4c343518f1606ff0ed5f924139f
Mario <100777733+Magocres@users.noreply.github.com>
2025-03-26 19:06:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_instance" "app_server" {
             # Clonar el repositorio de GitHub
             cd /home/ec2-user
             git clone https://github.com/Magocres/practica-tf.git
-            cd mi-app-tf
+            cd practica-tf
             
             # Construir y ejecutar el contenedor Docker
             docker build -t mi-aplicacion .
